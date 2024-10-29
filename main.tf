2849cdd4cbd8f34a0dcfcbe4326f3492a2c8e790
David Joo <david.joo@hashicorp.com>
2024-10-29 22:46:29 +1100

    removing separate namespace, it will use default "admin" namespace

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 ### Create a namespace
-resource "vault_namespace" "djoo-demo" {
-  path = "djoo-demo"
-}
+#resource "vault_namespace" "djoo-demo" {
+#  path = "djoo-demo"
+#}
 
 ### Create a KVv2 Secrets Engine
 resource "vault_mount" "kv-demo" {
-  namespace  = vault_namespace.djoo-demo.path
+#  namespace  = vault_namespace.djoo-demo.path
   path        = "kv-demo"
   type        = "kv"
   options     = { version = "2" }
@@ -13,7 +13,7 @@ resource "vault_mount" "kv-demo" {
 }
 
 resource "vault_kv_secret_backend_v2" "kv-demo-back" {
-  namespace  = vault_namespace.djoo-demo.path
+#  namespace  = vault_namespace.djoo-demo.path
   mount                = vault_mount.kv-demo.path
   max_versions         = 100
   #delete_version_after = 12600
@@ -22,7 +22,7 @@ resource "vault_kv_secret_backend_v2" "kv-demo-back" {
 
 ### Entries for the KVv2 Secrets Engine
 resource "vault_kv_secret_v2" "prod" {
-  namespace  = vault_namespace.djoo-demo.path
+#  namespace  = vault_namespace.djoo-demo.path
   mount = vault_mount.kv-demo.path
   name = "prod"
   cas = 1
@@ -35,7 +35,7 @@ resource "vault_kv_secret_v2" "prod" {
 }
 
 resource "vault_kv_secret_v2" "dev" {
-  namespace  = vault_namespace.djoo-demo.path
+#  namespace  = vault_namespace.djoo-demo.path
   mount = vault_mount.kv-demo.path
   name = "dev"
   cas = 1
