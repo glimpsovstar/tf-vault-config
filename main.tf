7cf1c63e54769d3037cb6c3d5614d81c910de499
David Joo <david.joo@hashicorp.com>
2024-10-29 22:28:15 +1100

    added kvv2 + some data

--- main.tf
+++ main.tf
@@ -1,3 +1,38 @@
+### Create a namespace
 resource "vault_namespace" "djoo-demo" {
   path = "djoo-demo"
 }
+
+### Create a KVv2 Secrets Engine
+resource "vault_secret_backend" "kv-demo" {
+  path = "djoo-demo/kv-demo"
+  type = "kv-v2"
+  description = "Demo KVv2 Secrets Engine"
+}
+
+### Entries for the KVv2 Secrets Engine
+resource "vault_kv_secret_v2" "prod" {
+  mount = vault_secret_backend.kv-demo.path
+  name = "prod"
+  cas = 1
+  data_json = jsonencode (
+        {
+            username = "p-admin",
+            password = "p*password"
+        }
+    )
+}
+
+resource "vault_kv_secret_v2" "dev" {
+  mount = vault_secret_backend.kv-demo.path
+  name = "dev"
+  cas = 1
+  data_json = jsonencode (
+        {
+            username = "d-admin",
+            password = "d^password"
+        }
+    )
+}
+
+
